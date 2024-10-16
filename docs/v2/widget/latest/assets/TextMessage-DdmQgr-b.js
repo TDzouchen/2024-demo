f3c74dee658107b1efbded94c27baea7ebab218b
TDzouchen <86048542+TDzouchen@users.noreply.github.com>
2024-10-16 16:42:46 +0800

    build

---
+++ docs/v2/widget/latest/assets/TextMessage-DdmQgr-b.js
@@ -0,0 +1,10 @@
+import{j as a,L as n}from"./main-vendor-N2Dp62kv.js";import{Q as i}from"./overlayscrollbars-g0MEiRao.js";import"./reporter-vendor-BrFA8t6T.js";/*!
+* Talkdesk Confidential
+*
+* Copyright (C) Talkdesk Inc. 2024
+*
+* The source code for this program is not published or otherwise divested
+* of its trade secrets, irrespective of what has been deposited with the
+* U.S. Copyright Office. Unauthorized copying of this file, via any medium
+* is strictly prohibited.
+*/const l="_text_133ru_1",c={text:l},f=t=>{const{className:e,text:o,linkProtocol:s,linkTarget:r}=t;return a.jsx(n,{as:"span",className:i(c.text,e),options:{defaultProtocol:s||void 0,target:r||void 0,rel:"noopener noreferrer"},children:o})};export{f as default};
---
+++ docs/v2/widget/latest/assets/TextMessage-legacy-BnvltTWd.js
@@ -0,0 +1,11 @@
+/*!
+* Talkdesk Confidential
+*
+* Copyright (C) Talkdesk Inc. 2024
+*
+* The source code for this program is not published or otherwise divested
+* of its trade secrets, irrespective of what has been deposited with the
+* U.S. Copyright Office. Unauthorized copying of this file, via any medium
+* is strictly prohibited.
+*/
+System.register(["./main-vendor-legacy-jrOTSjzZ.js","./overlayscrollbars-legacy-BkWmz4_1.js","./reporter-vendor-legacy-BVegT3cb.js"],(function(e,t){"use strict";var r,n,o;return{setters:[function(e){r=e.j,n=e.L},function(e){o=e.Q},null],execute:function(){var t=document.createElement("style");t.textContent="._text_133ru_1{overflow-wrap:anywhere;white-space:pre-line}\n",document.head.appendChild(t);var a="_text_133ru_1";e("default",(function(e){var t=e.className,l=e.text,s=e.linkProtocol,c=e.linkTarget;return r.jsx(n,{as:"span",className:o(a,t),options:{defaultProtocol:s||void 0,target:c||void 0,rel:"noopener noreferrer"},children:l})}))}}}));
