1a104f05364bf0d86861283522dc534df37908a3
TDzouchen <86048542+TDzouchen@users.noreply.github.com>
2024-10-16 15:56:26 +0800

    push

---
+++ docs/v2/widget/latest/assets/Buttons-legacy-Cjre4vrX.js
@@ -0,0 +1,11 @@
+/*!
+* Talkdesk Confidential
+*
+* Copyright (C) Talkdesk Inc. 2024
+*
+* The source code for this program is not published or otherwise divested
+* of its trade secrets, irrespective of what has been deposited with the
+* U.S. Copyright Office. Unauthorized copying of this file, via any medium
+* is strictly prohibited.
+*/
+System.register(["./main-vendor-legacy-jrOTSjzZ.js","./overlayscrollbars-legacy-BkWmz4_1.js","./reporter-vendor-legacy-BVegT3cb.js"],(function(n,t){"use strict";var e,r;return{setters:[function(n){e=n.j},function(n){r=n.O},null],execute:function(){n("default",(function(n){var t=n.buttons,c=n.onClick;return e.jsx("div",{className:r.container,children:t.map((function(n,t){return e.jsx("button",{className:r.button,"data-qa":"quick_action",onClick:function(){return c(n)},children:n.text},t)}))})}))}}}));
